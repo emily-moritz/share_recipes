d9a67f2b92a688cffd5022b824be93083e2b18e3
Emily Moritz <131187067+emily-moritz@users.noreply.github.com>
2023-05-09 21:58:01 +0200

    Dependencies hinzugefügt + aktueller Stand vom code

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,273 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
 
-void main() {
-  runApp(const MyApp());
+//import 'dart:developer'; //log
+
+class Steps {
+  final String title;
+  final String text;
+  final int ordernumber;
+
+  const Steps(this.title, this.text, this.ordernumber);
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Ingedients {
+  final String name;
+  final String amount;
 
-  // This widget is the root of your application.
+  const Ingedients(this.name, this.amount);
+}
+
+class Infos {
+  final String title;
+  final int id;
+  final String description;
+  final List<Steps> steps;
+  final List<Ingedients> ingedients;
+
+  const Infos(this.title, this.id, this.description, this.steps, this.ingedients);
+}
+
+List<Infos> allinfos = [];
+
+// searchbar
+// scroll down refresh
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget { //erbt von StatelessWidget
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Meine Rezepte',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Meine Rezepte'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Future getData() async {
+    var url = Uri.parse('http://512403920.swh.strato-hosting.eu/flutter/get2.php');
+    http.Response response = await http.get(url);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    //create Array
+    jsonDecode(response.body).forEach((key, value) {
+      int localid = 0;
+      String localtitle = "";
+      String localdescription= "";
+      List<Steps> allsteps = [];
+      List<Ingedients> allingedients = [];
+      value.forEach((key2, value2) {
+        if(key2 == 'id'){
+          localid = value2;
+        }
+        else if(key2 == 'title'){
+          localtitle = value2;
+        }
+        else if(key2 == 'description'){
+          localdescription = value2;
+        }
+        else{
+          value2.forEach((key3, value3) {
+            if(key2 == 'steps'){
+              String localtitle2 = "";
+              String localtext = "";
+              int localordernumber = 0;
+              value3.forEach((key4, value4) {
+                if(key4 == 'title'){
+                  localtitle2 = value4;
+                }
+                else if(key4 == 'text'){
+                  localtext = value4;
+                }
+                else if(key4 == 'order_number'){
+                  localordernumber = value4;
+                }
+              });
+              allsteps.add(Steps(localtitle2, localtext, localordernumber));
+            }
+            else if(key2 == 'ingedients'){
+              String localname = "";
+              String localamount = "";
+              value3.forEach((key4, value4) {
+                if(key4 == 'name'){
+                  localname = value4;
+                }
+                else if(key4 == 'amount'){
+                  localamount = value4;
+                }
+              });
+              allingedients.add(Ingedients(localname, localamount));
+            }
+          });
+        }
+      });
+      allinfos.add(Infos(localtitle, localid, localdescription, allsteps, allingedients));
     });
   }
 
   @override
+  void initState() {
+    getData();
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+  return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Meine Rezepte"),
+        centerTitle: true,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        children: <Widget>[
+          Align(
+          alignment: Alignment(0.85, 0.95),
+          child: ElevatedButton(
+            onPressed: () {}, 
+            style: ElevatedButton.styleFrom(
+              fixedSize: const Size(50, 50),
+              shape: const CircleBorder(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+            child: const Text(
+              'Rezept hinzufügen',
+              style: TextStyle(fontSize: 24),
+            )
+          )
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        ListView(
+          children: [
+            for(int i = 0; i < allinfos.length; i++)
+              ListTile(
+                title: Text(allinfos[i].title),
+                subtitle: Text(allinfos[i].description),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ReciptScreen(id: i)),
+                  );
+                },
+              ),
+          ],
+        )
+        ]
+      )
     );
+  }
+}
+
+/*
+
+Expanded(
+            child: Align(
+              alignment: Alignment(0.85, 0.95),
+              child: ElevatedButton(
+                onPressed: () {}, 
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(50, 50),
+                  shape: const CircleBorder(),
+                ),
+                child: const Text(
+                  '+',
+                  style: TextStyle(fontSize: 24),
+                )
+              )
+            )
+          )
+
+*/
+
+class ReciptScreen extends StatelessWidget {
+  final int id;
+  
+  const ReciptScreen({super.key, required this.id});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(allinfos[id].title),
+      ),
+      body: Center(
+        child: Column(children: [
+          Text(allinfos[id].description, style: const TextStyle(
+            fontSize: 25.0,
+            fontWeight: FontWeight.normal,
+            color: Colors.black)
+          ),
+          Text(" "),
+          if(allinfos[id].ingedients.isNotEmpty)
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children:[
+                Row(children:[
+                  Text("Zutaten:")   
+                ])
+              ]
+            ),
+            for(int i = 0; i < allinfos[id].ingedients.length; i++)
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children:[
+                  Row(children:[
+                    if(allinfos[id].ingedients[i].name.isNotEmpty && allinfos[id].ingedients[i].amount.isNotEmpty)
+                      Text(allinfos[id].ingedients[i].amount.toString()),
+                      const Text(" "),
+                      Text(allinfos[id].ingedients[i].name.toString()),
+                  ]),
+                ]
+              ),
+          
+          Text(" "),
+              
+          if(allinfos[id].steps.isNotEmpty)
+            for(int i = 0; i < allinfos[id].steps.length; i++)
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children:[
+                  Row(children:[
+                    if(allinfos[id].steps[i].title.isNotEmpty)
+                      Text("Schritt $i: " + allinfos[id].steps[i].title.toString())                    
+                  ]),
+                  Row(children:[
+                    if(allinfos[id].steps[i].text.isNotEmpty)
+                      Text(allinfos[id].steps[i].text.toString())
+                  ]),
+                  Row(children:[
+                    if(allinfos[id].steps[i].text.isNotEmpty)
+                      const Text(" ")
+                  ])
+                ]
+              ),      
+          Expanded(
+            child: Align(
+              alignment: const Alignment(0.0, 0.95),
+              child: ElevatedButton(
+                onPressed: () {}, 
+                child: const Text(
+                  'Bearbeiten',
+                  style: TextStyle(fontSize: 20),
+                )
+              )
+            )
+          )
+        ]),
+      ));
   }
 }
